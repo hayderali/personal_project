ea79282c183d28851f9dd6cd92e93f02c46eea22
Hayder Ali <hali@spartaglobal.com>
2015-12-05 18:18:10 +0000

    inheritance not working, working on rspec now

--- paint.rb
+++ paint.rb
@@ -1,28 +1,23 @@
 require 'green_shoes'
-require './protanopia'
-#store another brush
+#require './protanopia.rb'
+#main class 
  
-class Paint
+class Paint 
   attr_accessor :previous_pos_x,:previous_pos_y,:brush_size,:decrease_size,:increase_size,:clean_slate
+	
   #setting the window size   
   
   #normal paint
   Shoes.app title: "Paint Project",:width => 640, :height => 400 do
-	def initialize(x,y,b_size,increase_brush,decrease_brush,clean)
-		
-		previous_pos_x = x
-		previous_pos_y = y
-		brush_size = b_size
-		increase_size = increase_brush
-		decrease_size = decrease_brush
-		clean_slate = clean
-		
-		@a = Protanopia.new
-		@a.draw_screen
-		
-		
-	end
-		
+	def initialize(x,y,b_size,increase_brush,decrease_brush,clean)	
+		@previous_pos_x = x
+		@previous_pos_y = y
+		@brush_size = b_size
+		@increase_size = increase_brush
+		@decrease_size = decrease_brush
+		@clean_slate = clean
+	end 
+	
 	#brush variables
 	@previous_pos_x 
 	@previous_pos_y
@@ -33,15 +28,10 @@ class Paint
 	@decrease_size = button "Brush Size -"
 	@clean_slate = button "Wipe Page"
 			
-			
-		
 	#gui colours
-	def draw_screen
+	def draw_screen	
 		background "#FFFFFF"
 		strokewidth(1)
-		
-		
-
 		stroke("#000000")
 		#border
 		line(322,0,322,400)
@@ -55,9 +45,17 @@ class Paint
 		purple = rect 240, 360, 40, 40, :fill => "#8500F2"
 		rubber = rect 280, 360, 40, 40, :fill => "#B7C5C9"
 		
-		
+		#protanopia
+		red = rect 320, 360, 40, 40, :fill => "#8C5001"
+		orange = rect 360, 360, 40, 40, :fill => "#DBAD16"
+		yellow = rect 400, 360, 40,40, :fill => "#FAF9F7"
+		green = rect  440, 360, 40, 40, :fill => "#DFE62E"
+		blue = rect  480, 360, 40, 40, :fill => "#CCD1F0"			
+		navy_blue = rect 520, 360, 40, 40, :fill => "#2400F2"
+		purple = rect 560, 360, 40, 40, :fill => "#024694"
+		grey = rect 600, 360, 40, 40, :fill => "#B7C5C9"		
 	end
-			
+	
 	def button 
 		message = para "Brush size is: " + @brush_size.to_s
 		@increase_size.click do
@@ -76,34 +74,12 @@ class Paint
 			clear_right = rect 322, 45, 640, 315, :fill => "#FFFFFF" 
 		end
 	end
-			
-	def paint_normal 
-		animate do
-			#size is 8, resets to 8 if goes to low or to high
-			strokewidth(@brush_size)
-			@brush_size = 8 if @brush_size <= 0
-			@brush_size = 8 if @brush_size >= 32
-			button, x,y = self.mouse
-			#if button is pressed between the frame, it will draw the line. 
-			if button == 1
-				if (@previous_pos_x > 2 && @previous_pos_x < 316) && (@previous_pos_y > 60 && @previous_pos_y < 360) && (x > 0 && x < 320) && (y > 60 && y < 360)
-
-					colour1 = line(@previous_pos_x, @previous_pos_y, x,y)
-					#colour2 = line(@previous_pos_x+320, @previous_pos_y, x+320,y)  
-					#c 
-				end
-			end
-			
-			@previous_pos_x = x
-			@previous_pos_y = y
-		end  
-	end
-			
+	
 	def choose_colours
 		click do |button, x, y|
 			if button == 1
-				#color
-				if (y > 360 && y < 400) && (x > 0 && x < 40)
+				#colour
+				if (y > 360 && y < 400) && (x > 0 && x < 40)			
 					stroke "#E30B0B"
 					puts "brush is now red"
 				end
@@ -139,27 +115,38 @@ class Paint
 					puts "brush is now purple"              
 				end	
 					
-				#eraser
+				#grey colour
 				if (y > 360 && y < 400) && (x > 280 && x < 320)
 					stroke "#B7C5C9"
 					puts "brush is now grey"
 				end	         
 			end
 		end  
-	end 
-    
-  draw_screen
-  choose_colours
-  button
-  paint_normal
-  
-  @proto = Protanopia.new
-	@proto.draw_screen
-  #@proto.choose_colours
-  #@proto.button
-  #@proto.paint_normal
-		
-  end    
+	end
+	
+	def paint_normal 
+		animate do
+			#size is 8, resets to 8 if goes to low or to high
+			strokewidth(@brush_size)
+			@brush_size = 8 if @brush_size <= 0
+			@brush_size = 8 if @brush_size >= 32
+			button, x,y = self.mouse
+			#if button is pressed between the frame, it will draw the line. 
+			if button == 1
+				if (@previous_pos_x > 2 && @previous_pos_x < 316) && (@previous_pos_y > 60 && @previous_pos_y < 360) && (x > 0 && x < 320) && (y > 60 && y < 360)
+						a =	line(@previous_pos_x, @previous_pos_y, x,y)						
+						b	= line(@previous_pos_x +320, @previous_pos_y, x+320,y)
+				end
+			end
+			@previous_pos_x = x
+			@previous_pos_y = y
+		end  
+	end
+			
+	draw_screen
+	button
+	choose_colours
+	paint_normal
+	
+  end 
 end
-
-
